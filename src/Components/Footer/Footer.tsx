7ffc8ea70a9ad5a9f1e6f5614a029069a268ff7f
Luan <luan@riangle.com>
2020-04-10 10:39:33 +0200

    🐛 FIX: Typo

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -8,7 +8,7 @@ function Footer() {
 			<p className={styles.small}>
 				Ovo je web-stranica sa otvorenim kodom za praćenje epidemije
 				COVID-19 u Crnoj Gori. Podaci potječu iz miješanog niza izvora,
-				uključujući službenu statistiku Instituta za javno zdravstvo
+				uključujući službenu statistiku Instituta za javno zdravlje
 				Crne Gore (IJZCG).
 			</p>
 			<p className={styles.small}>

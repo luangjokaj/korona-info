4ff4d4fb3759357e944f51bfefd88254ffd718a9
Luan Gjokaj <luan@riangle.com>
2020-08-16 16:25:29 +0200

    🚀 RELEASE: Update dependencies

--- src/local-data/summary.ts
+++ src/local-data/summary.ts
@@ -1,8 +1,8 @@
 export const localData = {
-	cases: 2893,
-	deaths: 45,
-	recovered: 809,
-	hospitalized: 188,
-	surveillanced: 3846,
-	tests: 32934,
+	cases: 3960,
+	deaths: 75,
+	recovered: 2830,
+	hospitalized: 142,
+	surveillanced: 2172,
+	tests: 41951,
 };
